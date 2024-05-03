7ff0c20e0c37e72ea589b192d8a48a2675d1aa33
Ville Saarenmaa <villethefin@gmail.com>
2024-05-03 14:26:13 +0300

    textnode to htmlnode done

--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,5 @@
+from leafnode import LeafNode
+
 class TextNode:
 
     def __init__(self, text, text_type, url=None):
@@ -11,3 +13,25 @@ class TextNode:
     def __repr__(self):
         teksti =  f"TextNode({self.text}, {self.text_type}, {self.url})"
         return teksti
+    
+    def text_node_to_html_node(self):
+        if self.text_type == "text":
+            self = LeafNode(None,self.text)
+
+        if self.text_type == "bold":
+            self = LeafNode("b", self.text)
+        
+        if self.text_type == "italic":
+            self = LeafNode("i", self.text)
+        
+        if self.text_type == "code":
+            self = LeafNode("code", self.text)
+        
+        if self.text_type == "link":
+            self = LeafNode("a", self.text, {"href":self.url})
+        
+        if self.text_type == "image":
+            self = LeafNode("img", "", {"src":self.url, "alt":self.text})
+
+        raise Exception("texttype not found")
+            
